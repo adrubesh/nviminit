8f294479ec10b3432231e830f8a62c33063a815d
adrubesh <contact@aaronrubesh.io>
2021-03-22 08:42:49 -0500

    added zsh and nvim config options

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# NVIM_LISTEN_PORT: Port for Neovim server to listen on
+# USER_ID: New userid for user
+# ZSH_CONFIG_URL: Url to download a ZSH config from
+
 function __nvim_listen() {
 	if [ -z "$NVIM_LISTEN_PORT" ]; then
 		printf "Please set %s to valid port." "NVIM_LISTEN_PORT" 
@@ -9,4 +13,28 @@ function __nvim_listen() {
 	nvim --listen "0.0.0.0:$NVIM_LISTEN_PORT" --headless
 }
 
+function __check_uid() {
+	CURRENT_UID=`id -u`
+	CURRENT_USER=`whoami`
+	if [ "$CURRENT_UID" -ne "$USER_ID"]; then
+		printf "Changing user id."
+		sudo usermod -u "$USER_ID" "$CURRENT_USER"
+	fi
+}
+
+function __check_zsh() {
+	if [ -v ZSH_CONFIG_URL && ! -f ~/.zshrc ]; then
+		curl -o ~/.zshrc -L "$ZSH_CONFIG_URL"
+	fi
+}
+
+function __check_nvim() {
+	if [ -v NVIM_CONFIG_URL && ! -f ~/.config/nvim/init.vim ]; then
+		curl -o ~/.config/nvim/init.vim --create-dirs -L "$NVIM_CONFIG_URL"
+	fi
+}
+
+__check_zsh
+__check_nvim
+__check_uid
 __nvim_listen
